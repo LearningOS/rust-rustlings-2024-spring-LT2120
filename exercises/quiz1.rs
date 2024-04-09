ef2cb0d1a192678a8c7a3c32f78cb4d4fe4e6e47
LT2120 <77767699+LT2120@users.noreply.github.com>
2024-04-09 03:35:57 +0000

    modified:   exercises/primitive_types/primitive_types1.rs 	modified:   exercises/primitive_types/primitive_types2.rs 	modified:   exercises/primitive_types/primitive_types3.rs 	modified:   exercises/primitive_types/primitive_types4.rs 	modified:   exercises/primitive_types/primitive_types5.rs 	modified:   exercises/primitive_types/primitive_types6.rs 	modified:   exercises/quiz1.rs

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(i:i32)->i32 {
+    if i>40{
+        i
+    }else{
+        2*i
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
